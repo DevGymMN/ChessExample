505dc097cf96fac98e67e3f8717ff99030169757
Brice Maltby <brice.maltby@gmail.com>
2019-05-29 23:07:11 -0500

    Simplify run.py and use UCI format moves

--- run.py
+++ run.py
@@ -1,14 +1,8 @@
-from random import randint
-import chess.pgn
-import sys
-import io
+import sys, chess.pgn, random, io
 
-def main():
-    pgn = io.StringIO(sys.argv[1])
-    game = chess.pgn.read_game(pgn)
-    board = game.board()
-    legal_moves = board.legal_moves
-    random_move = list(legal_moves)[(randint(0,legal_moves.count()-1))]
-    print(random_move)
+pgn = io.StringIO(sys.argv[1])
+game = chess.pgn.read_game(pgn)
+board = game.board()
+move = random.sample(list(board.legal_moves), 1)[0]
 
-main()
+print(move.uci())
